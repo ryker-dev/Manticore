4dfaccacc4dfb85b393ebda45be3ae4c3be95702
EmilioPizzuti <pizzuti.dev@gmail.com>
2021-07-05 14:23:45 +0300

    Working on the front page

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set initial state
+let menuOpen = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (!menuOpen) {
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        navItems.forEach(item => item.classList.add("show"));
+
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        navItems.forEach(item => item.classList.remove("show"));
+
+        menuOpen = false;
+    }
+}
